acfbb1d108bcb09e06b130b0fc00b28a6158b8fa
Marcus Gonçalves <marcus.goncalvezz@outlook.com>
2020-08-08 00:46:50 -0300

    mobile with react native

---
+++ mobile/App.tsx
@@ -0,0 +1,27 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Olá!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#333",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  title: {
+    color: "#FFF",
+    fontSize: 32,
+    fontWeight: "bold",
+  },
+});
