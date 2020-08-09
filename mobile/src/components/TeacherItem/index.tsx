d73101f460543ee266dadd8005e3b6d79ebb5fc1
Marcus Gonçalves <marcus.goncalvezz@outlook.com>
2020-08-08 21:34:40 -0300

    styled TeacherItem

---
+++ mobile/src/components/TeacherItem/index.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { View, Image, Text } from "react-native";
+import { RectButton } from "react-native-gesture-handler";
+
+import heartOutlineIcon from "../../assets/images/icons/heart-outline.png";
+import unfavoriteIcon from "../../assets/images/icons/unfavorite.png";
+import whatsappIcon from "../../assets/images/icons/whatsapp.png";
+
+import styles from "./styles";
+
+function TeacherItem() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.profile}>
+        <Image
+          style={styles.avatar}
+          source={{ uri: "https://github.com/marcusGoncalvess.png" }}
+        />
+
+        <View style={styles.profileInfo}>
+          <Text style={styles.name}>Marcus Gonçalves</Text>
+          <Text style={styles.subject}>Filosofia</Text>
+        </View>
+      </View>
+
+      <Text style={styles.bio}>Só sei que nada sei</Text>
+
+      <View style={styles.footer}>
+        <Text style={styles.price}>
+          Preço/hora {"  "}
+          <Text style={styles.priceValue}>R$ 20,00</Text>
+        </Text>
+
+        <View style={styles.buttonsContainer}>
+          <RectButton style={styles.favoriteButton}>
+            <Image source={heartOutlineIcon} />
+          </RectButton>
+
+          <RectButton style={styles.contactButton}>
+            <Image source={whatsappIcon} />
+            <Text style={styles.contactButtonText}>Entrar em contato</Text>
+          </RectButton>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+export default TeacherItem;
--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -4,11 +4,13 @@ import { View } from "react-native";
 import styles from "./styles";
 
 import PageHeader from "../../components/PageHeader";
+import TeacherItem from "../../components/TeacherItem";
 
 function TeacherList() {
   return (
   <View style={styles.container} >
     <PageHeader title={"Proffys disponíveis"}/>
+    <TeacherItem />
   </View>
   );
 }
